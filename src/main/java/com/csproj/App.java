c585ab93f39a6f029f325d43669953821dcc0861
mhaider <mshaider3@gmail.com>
2025-11-25 16:58:54 -0600

    Completed entire registration for agent and renter.

--- src/main/java/com/csproj/App.java
+++ src/main/java/com/csproj/App.java
@@ -19,43 +19,27 @@ public class App
                 String user = scanner.next();
                 if (user.equals("A") || user.equals("a")) {
                     // Call the class that will prompt the agent account
-                    System.out.print("Enter your first name:");
-                    String firstName = scanner.nextLine();
-                    System.out.print("Enter your last name: ");
-                    String lastName = scanner.nextLine();
-                    System.out.print("Enter email: ");
-                    String email = scanner.nextLine();
-                    System.out.print("Enter job title: ");
-                    String jobTitle = scanner.nextLine();
-                    System.out.print("Enter agency name: ");
-                    String agency = scanner.nextLine();
-                    System.out.print("Enter phone number: ");
-                    Integer phoneNum = scanner.nextInt();
-                    scanner.nextLine();
+                    System.out.print("Enter your first name: "); System.out.flush(); String firstName = scanner.next(); scanner.nextLine();
+                    System.out.print("Enter your last name: "); System.out.flush(); String lastName = scanner.next(); scanner.nextLine();
+                    System.out.print("Enter email: "); System.out.flush(); String email = scanner.nextLine();
+                    System.out.print("Enter job title: "); System.out.flush(); String jobTitle = scanner.nextLine();
+                    System.out.print("Enter agency name: "); System.out.flush(); String agency = scanner.nextLine();
+                    System.out.print("Enter phone number: "); System.out.flush(); String phoneNum = scanner.nextLine(); 
                     Registration.registerUser(email, firstName, lastName);
                     Registration.registerAgent(email, jobTitle, agency, phoneNum);
                 } else if (user.equals("R") || user.equals("r")) {
                     // Call the class that will prompt the renter account
-                    System.out.print("Enter your first name:");
-                    String firstName = scanner.nextLine();
-                    System.out.print("Enter your last name: ");
-                    String lastName = scanner.nextLine();
-                    System.out.print("Enter email: ");
-                    String email = scanner.nextLine();
-                    System.out.print("Enter your agent's ID: ");
-                    Integer agentID = scanner.nextInt();
-                    System.out.print("Enter your agent's email: ");
-                    String agentEmail = scanner.nextLine();
-                    System.out.print("Enter your desired property size: ");
-                    Integer size = scanner.nextInt();
-                    System.out.print("Enter your desired maximum room number: ");
-                    Integer maxRoom = scanner.nextInt();
-                    System.out.print("Enter your budget: ");
-                    String budgetVal = scanner.nextLine();
-                    BigDecimal budget = new BigDecimal(budgetVal);
-                    System.out.print("Enter your desired move in date (YEAR-MONTH-DAY format): ");
-                    String date = scanner.nextLine();
-                    scanner.nextLine();
+                    System.out.print("Enter your first name: "); System.out.flush(); String firstName = scanner.next(); scanner.nextLine();
+                    System.out.print("Enter your last name: "); System.out.flush(); String lastName = scanner.next(); scanner.nextLine();
+                    System.out.print("Enter email: "); System.out.flush(); String email = scanner.nextLine();
+                    System.out.println("Here are the options for agents.");
+                    Registration.agentOptions();
+                    System.out.print("Enter your agent's ID: "); System.out.flush(); String agentID = scanner.nextLine();
+                    System.out.print("Enter your agent's email: "); System.out.flush(); String agentEmail = scanner.nextLine();
+                    System.out.print("Enter your desired property size: "); System.out.flush(); String size = scanner.nextLine();
+                    System.out.print("Enter your desired maximum room number: "); System.out.flush(); String maxRoom = scanner.nextLine();
+                    System.out.print("Enter your budget: "); System.out.flush(); String budget = scanner.nextLine();
+                    System.out.print("Enter your desired move in date (YEAR-MONTH-DAY format): "); System.out.flush(); String date = scanner.nextLine();
                     Registration.registerUser(email, firstName, lastName);
                     Registration.registerRenter(email, agentID, agentEmail, size, maxRoom, budget, date);
                 }
--- src/main/java/com/csproj/Registration.java
+++ src/main/java/com/csproj/Registration.java
@@ -1,15 +1,15 @@
 package com.csproj;
+import java.sql.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
 
 public class Registration {
     
-    public static void registerUser(String email, String firstName, String lastName) {
+    public static void registerUser(String uEmail, String firstName, String lastName) {
         Properties props = new Properties();
         try {
             props.load(new FileInputStream("/Users/maz/Desktop/CS425/real-estate-application/real-estate-app/src/main/java/com/csproj/dbproperties"));
@@ -23,18 +23,23 @@ public class Registration {
         String password = props.getProperty("db.password");
 
         // SQL insert query for inserting data
-        String insertUser = "INSERT INTO user ";
-        
+        String insertUser = "INSERT INTO \"user\" (email, first_name, last_name) " 
+                            + "VALUES ('" + uEmail + "', '" + firstName + "', '" + lastName + "');";
+        System.out.println(insertUser);
+         
         // Connect to the db and insert the user data
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected to the database!");
+        try (Connection conn = DriverManager.getConnection(url, user, password); Statement stmt = conn.createStatement();) {
+            conn.setAutoCommit(false);
+            //System.out.println("Connected to the database!");
+            stmt.executeUpdate(insertUser);
+            conn.commit();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         System.out.println("User record created successfully.");
     }
 
-    public static void registerAgent(String email, String jobTitle, String agency, Integer phoneNum) {
+    public static void registerAgent(String uEmail, String jobTitle, String agency, String phoneNum) {
         Properties props = new Properties();
         try {
             props.load(new FileInputStream("/Users/maz/Desktop/CS425/real-estate-application/real-estate-app/src/main/java/com/csproj/dbproperties"));
@@ -47,14 +52,48 @@ public class Registration {
         String user = props.getProperty("db.user");
         String password = props.getProperty("db.password");
 
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected to the database!");
+        String insertAgent = "INSERT INTO agent (email, job_title, agency, phone_number) "
+                            + "VALUES ('" + uEmail + "', '" + jobTitle + "', '" + agency + "', " + phoneNum + ");";
+        System.out.println(insertAgent);
+        try (Connection conn = DriverManager.getConnection(url, user, password); Statement stmt = conn.createStatement();) {
+            conn.setAutoCommit(false);
+            //System.out.println("Connected to the database!");
+            stmt.executeUpdate(insertAgent);
+            conn.commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Agent record created successfully.");
+    }
+
+    public static void registerRenter(String uEmail, String agentID, String agentEmail, String size, String maxRoom, String budget, String date) {
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream("/Users/maz/Desktop/CS425/real-estate-application/real-estate-app/src/main/java/com/csproj/dbproperties"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        String userRenter = "INSERT INTO prospective_renter (email, agent_id, agent_email, desired_size, max_room_number, budget, desired_movein_date) "
+                        + "VALUES ('" + uEmail + "', " + agentID + ", '" + agentEmail + "', " + size + ", " + maxRoom + ", " + budget + ", '" + date + "');";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password); Statement stmt = conn.createStatement();) {
+            conn.setAutoCommit(false);
+            //System.out.println("Connected to the database!");
+            stmt.executeUpdate(userRenter);
+            conn.commit();
         } catch (Exception e) {
             System.out.println("Failed to make connection!");
         }
+        System.out.println("Renter update successful.");
     }
 
-    public static void registerRenter(String email, Integer agentID, String agentEmail, Integer size, Integer maxRoom, BigDecimal budget, String date) {
+    public static void agentOptions() {
         Properties props = new Properties();
         try {
             props.load(new FileInputStream("/Users/maz/Desktop/CS425/real-estate-application/real-estate-app/src/main/java/com/csproj/dbproperties"));
@@ -67,8 +106,15 @@ public class Registration {
         String user = props.getProperty("db.user");
         String password = props.getProperty("db.password");
 
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected to the database!");
+        String possibleAgents = "SELECT agent_id, email FROM agent";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password); Statement stmt = conn.createStatement();) {
+            //System.out.println("Connected to the database!");
+            ResultSet rset = stmt.executeQuery(possibleAgents);
+            while (rset.next()) {
+                System.out.println("Agent ID: " + rset.getString("agent_id") 
+                + ", Agent email: " + rset.getString("email"));
+            }
         } catch (Exception e) {
             System.out.println("Failed to make connection!");
         }
