4a32345d1039a024481f552e655e10ab2d3a6739
mhaider <mshaider3@gmail.com>
2025-11-24 23:10:04 -0600

    Removed url, login, pass from the App.java file and placed it in configuration file.

--- src/main/java/com/csproj/App.java
+++ src/main/java/com/csproj/App.java
@@ -1,14 +1,28 @@
 package com.csproj;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        String url = "jdbc:postgresql://localhost:5432/RealEstateDB";
-        String user = "postgres";
-        String password = "cs425class";
+
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream("src/main/java/com/csproj/db.properties"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connected to the database!");
