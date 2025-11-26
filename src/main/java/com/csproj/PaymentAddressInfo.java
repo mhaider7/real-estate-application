b2c8b6c646cfd661de0227280b3ae4172f98aa50
mhaider <mshaider3@gmail.com>
2025-11-25 23:50:30 -0600

    Layout for address & cc info

---
+++ src/main/java/com/csproj/PaymentAddressInfo.java
@@ -0,0 +1,16 @@
+package com.csproj;
+
+public class PaymentAddressInfo {
+    
+    public static void add() {
+        System.out.println();
+    }
+
+    public static void modify() {
+        System.out.println();
+    }
+
+    public static void delete() {
+        System.out.println();
+    }
+}
